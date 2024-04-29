6b32f2635b083f9ebe290be2a3eb44b7981346dd
Radosław Niżnik <radekniznik57@gmail.com>
2024-04-29 12:57:10 +0200

    make simple routing and navbar

---
+++ app/(tabs)/reservation.tsx
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native'
+
+import EditScreenInfo from '@/components/EditScreenInfo'
+import { Text, View } from '@/components/Themed'
+import { Link } from 'expo-router'
+
+export default function Reservations() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Reservations</Text>
+            <View
+                style={styles.separator}
+                lightColor="#eee"
+                darkColor="rgba(255,255,255,0.1)"
+            />
+            <Text>
+                Choose a room:
+                <Link href="/reservation/1">Room 1</Link>
+                <Link href="/reservation/2">Room 2</Link>
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: '80%',
+    },
+})
