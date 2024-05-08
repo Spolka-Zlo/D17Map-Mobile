81094285372f1766911d5a67167018cb9762e1ae
irosikoni <irosikoni@gmail.com>
2024-05-08 12:32:56 +0200

    stop propagation added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,15 +7,23 @@ import { useState } from 'react'
 import Colors from '@/constants/Colors'
 
 export default function TabOneScreen() {
-    const [selected, setSelected] = useState("Option 1");
+    const [selected, setSelected] = useState('Option 1')
+    const [isOpen, setIsOpen] = useState(false)
     return (
-        <View style={styles.container}>
+        <View
+            style={styles.container}
+            onTouchStart={() => {
+                setIsOpen(false)
+            }}
+        >
             <ListElement text="Event number one" onPress={() => {}} />
             <OrangeButton text="Click Me" onPress={() => {}} />
             <Dropdown
-                options={["Option 1", "Option 2", "Option 3"]}
+                options={['Option 1', 'Option 2', 'Option 3']}
                 selected={selected}
                 setSelected={setSelected}
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
             />
         </View>
     )
