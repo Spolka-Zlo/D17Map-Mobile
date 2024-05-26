83107e8b011b4abcfa9ad9e21d486e511428d738
Radosław Niżnik <radekniznik57@gmail.com>
2024-05-26 21:17:17 +0200

    add timePicker

--- app/(tabs)/reservation/newReservation.tsx
+++ app/(tabs)/reservation/newReservation.tsx
@@ -1,27 +1,51 @@
-import { StyleSheet, View, Text, ScrollView, TextInput } from 'react-native'
+import {
+    StyleSheet,
+    View,
+    Text,
+    ScrollView,
+    TextInput,
+    Button,
+} from 'react-native'
 import { Styles } from '@/constants/Styles'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import Calendar from '@/components/Calendar';
-import { useState } from 'react';
-import RoomDropdown from '@/components/RoomDropdown';
+import Calendar from '@/components/Calendar'
+import { useState } from 'react'
+import RoomDropdown from '@/components/RoomDropdown'
+import TimePicker from '@/components/TimePicker'
+import Colors from '@/constants/Colors'
 
 export default function Reservations() {
     const [selectedDate, setSelectedDate] = useState<Date>(new Date())
     const [selectedRooms, setSelectedRooms] = useState<string[]>([])
+    const [startTime, setStartTime] = useState<string>('')
+    const [endTime, setEndTime] = useState<string>('')
 
     return (
-        <SafeAreaView style={Styles.background}>
-            <Text style={[Styles.h1, styles.h1]}>Nowa rezerwacja</Text>
-            <Calendar setSelectedDate={setSelectedDate} />
-            <Text>{selectedDate.toDateString()}</Text>
-            <Text>{selectedRooms}</Text>
-            <RoomDropdown setSelectedRooms={setSelectedRooms} />
-        </SafeAreaView>
+        <ScrollView style={styles.background}>
+            <SafeAreaView style={Styles.background}>
+                <Text style={[Styles.h1, styles.h1]}>Nowa rezerwacja</Text>
+                <Calendar setSelectedDate={setSelectedDate} />
+                <Text>{selectedDate.toDateString()}</Text>
+                <Text>{selectedRooms}</Text>
+                <RoomDropdown setSelectedRooms={setSelectedRooms} />
+                <TimePicker
+                    startTime={startTime}
+                    endTime={endTime}
+                    setStartTime={setStartTime}
+                    setEndTime={setEndTime}
+                />
+                <Text>{startTime}</Text>
+                <Text>{endTime}</Text>
+            </SafeAreaView>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     h1: {
         marginTop: -50,
+    },
+    background: {
+        backgroundColor: Colors.mapGrey,
     },
 })
---
+++ components/TimePicker.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react'
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+import Colors from '@/constants/Colors'
+import RNDateTimePicker from '@react-native-community/datetimepicker'
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '95%',
+        padding: 16,
+        marginTop: -10,
+    },
+    picker: {
+        backgroundColor: Colors.primary,
+        padding: 10,
+        margin: 10,
+        width: '100%',
+        borderRadius: 10,
+    },
+    text: {
+        color: Colors.secondary,
+        fontWeight: 'bold',
+        fontSize: 16,
+        textAlign: 'center',
+    },
+})
+
+type TimePickerProps = {
+    startTime: string
+    endTime: string
+    setStartTime: (time: string) => void
+    setEndTime: (time: string) => void
+}
+
+export default function TimePicker({
+    startTime,
+    endTime,
+    setStartTime,
+    setEndTime,
+}: TimePickerProps) {
+    const [startPickShow, setStartShow] = useState(false)
+    const [endPickShow, setEndShow] = useState(false)
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity
+                style={styles.picker}
+                onPress={() => setStartShow(true)}
+            >
+                <Text style={styles.text}>
+                    {startTime || 'Wskaż godzinę rozpoczęcia'}
+                </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.picker}
+                onPress={() => setEndShow(true)}
+            >
+                <Text style={styles.text}>
+                    {endTime || 'Wskaż godzinę zakończenia'}
+                </Text>
+            </TouchableOpacity>
+            {startPickShow && (
+                <RNDateTimePicker
+                    value={new Date()}
+                    mode="time"
+                    display="default"
+                    is24Hour={true}
+                    onChange={(event, selectedDate) => {
+                        setStartShow(false)
+                        setStartTime(
+                            selectedDate?.toLocaleTimeString([], {
+                                hour: '2-digit',
+                                minute: '2-digit',
+                                hour12: false,
+                            }) || ''
+                        )
+                    }}
+                />
+            )}
+            {endPickShow && (
+                <RNDateTimePicker
+                    value={new Date()}
+                    mode="time"
+                    display="default"
+                    is24Hour={true}
+                    onChange={(event, selectedDate) => {
+                        setEndShow(false)
+                        setEndTime(
+                            selectedDate?.toLocaleTimeString([], {
+                                hour: '2-digit',
+                                minute: '2-digit',
+                                hour12: false,
+                            }) || ''
+                        )
+                    }}
+                />
+            )}
+        </View>
+    )
+}
