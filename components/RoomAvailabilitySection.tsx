8f1f901959009de9ce23d966ab78a525009157ef
yoinker57 <radekniznik57@gmail.com>
2024-10-24 22:21:35 +0200

    change import

--- components/RoomAvailabilitySection.tsx
+++ components/RoomAvailabilitySection.tsx
@@ -3,8 +3,9 @@ import { useState } from 'react'
 import Colors from '@/constants/Colors'
 import RoomDropdown from './RoomDropdown'
 import TimeSlotPicker from './TimeSlotPicker'
-import { DayReservation, Room } from '@/app/(tabs)/reservation/newReservation'
+import { DayReservation } from '@/app/(tabs)/reservation/newReservation'
 import CompleteReservationPopUp from './CompleteReservationPopUp'
+import { Room } from '@/constants/types'
 
 type RoomAvailabilitySectionProps = {
     reservations: DayReservation[]
