8a040067019a41c2098a4d5ee71c5bba3ed7697a
yoinker57 <radekniznik57@gmail.com>
2024-11-01 15:46:28 +0100

    some lint fixes

--- app/(tabs)/map/map.tsx
+++ app/(tabs)/map/map.tsx
@@ -1,17 +1,7 @@
-import React, { Suspense, useEffect } from 'react'
-import { useFrame, Canvas } from '@react-three/fiber/native'
-import { useGLTF, Environment } from '@react-three/drei/native'
-import Floor1 from '@/assets/models/floor1.glb'
-import Floor2 from '@/assets/models/floor2.glb'
-import Dropdown from '@/components/Dropdown'
+import React from 'react'
 import { View, Text } from 'react-native'
-import { Link, router } from 'expo-router'
+import { Link } from 'expo-router'
 
-
-const models: Record<string, string> = {
-    'floor1': Floor1,
-    'floor2': Floor2,
-}
 
 export default function Map() {
 
