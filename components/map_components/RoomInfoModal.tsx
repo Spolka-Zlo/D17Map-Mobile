d352e8b907ffee28ed446d01add9a33f7c54731e
yoinker57 <radekniznik57@gmail.com>
2024-11-03 23:22:52 +0100

    add normal photo - spheric photo

--- components/map_components/RoomInfoModal.tsx
+++ components/map_components/RoomInfoModal.tsx
@@ -1,8 +1,8 @@
 import Colors from '@/constants/Colors'
 import { Equipment, Room } from '@/constants/types'
 import { useEquipmentOptions } from '@/services/classroomService'
-import { View, Text, StyleSheet,  TouchableOpacity } from 'react-native'
-import SpherePhoto from '../spherephoto_components/SpherePhoto'
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
+// import SpherePhoto from '../spherephoto_components/SpherePhoto'
 
 type RoomInfoModalProps = {
     onClose: () => void
@@ -20,9 +20,10 @@ export const RoomInfoModal = (props: RoomInfoModalProps) => {
         return (
             <View style={styles.container}>
                 <View style={styles.innerContainer}>
-                    <View style={styles.placeholder}>
+                    {/* <View style={styles.placeholder}>
                         <SpherePhoto/>
-                    </View>
+                    </View> */}
+                    <Image source={{ uri: 'http://192.168.33.12:8080/classrooms/1/photo.jpg' }} style={styles.image} />
                     <Text style={styles.title}>{props.room.name}</Text>
                     <Text>Pojemność sali: {props.room.capacity}</Text>
                     <Text>Wyposażenie: {roomEquipment || 'Brak wyposażenia'}</Text>
@@ -58,6 +59,11 @@ const styles = StyleSheet.create({
         height: 400,
         width: '100%',
         zIndex: 12,
+    },
+    image:{
+        width: '100%',
+        height: 400,
+        borderRadius: 10,
     },
     title: {
         fontSize: 18,
