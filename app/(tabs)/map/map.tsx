aaec1b20ac42c4959a18f5029e6e6e8a15205f67
yoinker57 <radekniznik57@gmail.com>
2024-11-16 13:44:18 +0100

    link to floor 1 map in tab

--- app/(tabs)/map/map.tsx
+++ app/(tabs)/map/map.tsx
@@ -1,17 +1,9 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import { Link } from 'expo-router'
+import {  router } from 'expo-router'
 
 export default function Map() {
+    router.push('/(tabs)/map/1')
+
     return (
-        <View style={{backgroundColor: "white"}}>
-            <Text >Reservations</Text>
-            <View/>
-            <Text>
-                Choose a room:
-                <Link href="/(tabs)/map/1">Floor 1</Link>
-                <Link href="/(tabs)/map/2">Floor 2</Link>
-            </Text>
-        </View>
+        null
     )
 }
